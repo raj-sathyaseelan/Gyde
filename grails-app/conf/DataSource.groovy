4f007788c652b4db7b7cfe88584577a57a9551eb
saravanaraj.sathya@gmail.com <saravanaraj.sathya@gmail.com>
2014-05-03 17:21:32 -0700

    fixed db issues

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,3 +1,32 @@
+ataSource {
+	pooled = true
+	//dbCreate = "update"
+	driverClassName = "com.mysql.jdbc.Driver"
+	dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+
+	properties {
+	   jmxEnabled = true
+	   initialSize = 5
+	   maxActive = 50
+	   minIdle = 5
+	   maxIdle = 25
+	   maxWait = 10000
+	   maxAge = 10 * 60000
+	   timeBetweenEvictionRunsMillis = 5000
+	   minEvictableIdleTimeMillis = 60000
+	   validationQuery = "SELECT 1"
+	   validationQueryTimeout = 3
+	   validationInterval = 15000
+	   testOnBorrow = true
+	   testWhileIdle = true
+	   testOnReturn = false
+	   jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+	   defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+	}
+
+}
+
+/*
 dataSource {
     pooled = true
     jmxExport = true
@@ -5,11 +34,13 @@ dataSource {
     username = "sa"
     password = ""
 }
+*/
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+	//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
     singleSession = true // configure OSIV singleSession mode
 }
 
@@ -18,19 +49,24 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:mysql://localhost:3306/firefly_health"
+			username = "root"
+			password = "password"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            //url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:mysql://localhost:3306/firefly_health_test"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            //url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+			url = "jdbc:mysql://localhost:3306/firefly_health_prod"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
@@ -52,5 +88,7 @@ environments {
                defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
             }
         }
+
     }
+
 }
